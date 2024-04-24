0a99654a6bef0e6d4cafbd087a411e1bd4111cdf
dmcnero123 <jair_duran@usmp.pe>
2024-04-24 11:44:11 -0500

    subiendo contacto

--- Data/Migrations/20240424162036_ContactoMigracion.Designer.cs
+++ Data/Migrations/20240424162036_ContactoMigracion.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using agendarauth.Data;
@@ -11,9 +12,11 @@ using agendarauth.Data;
 namespace agendarauth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240424162036_ContactoMigracion")]
+    partial class ContactoMigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
